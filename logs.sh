88721efb0008ec0cdf0d98cd30291ce41acd7526
Jonathan Brinley <jbrinley@flightless.us>
2016-08-23 13:36:22 -0400

    fix error in logs script

--- logs.sh
+++ logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-REPODIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+REPODIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd "$REPODIR/global"
 
-docker-compose --project-name=global logs
+docker-compose --project-name=global logs --follow
